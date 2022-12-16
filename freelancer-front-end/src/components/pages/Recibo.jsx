94ece8b68dce26e6a061e044f584aca309608c96
Jasem Duncan <jasemvalencia@gmail.com>
2022-12-15 23:33:16 -0500

    Create structure and components

---
+++ freelancer-front-end/src/components/pages/Recibo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Recibo = () => {
+  return (
+    <div>Recibo</div>
+  )
+}
---
+++ freelancer-front-end/src/components/pages/Recibos.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Recibos = () => {
+  return (
+    <div>Recibos</div>
+  )
+}
