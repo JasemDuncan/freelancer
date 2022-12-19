15aec33845396d5f5b393367bb90d48674e5c842
Jasem Duncan <jasemvalencia@gmail.com>
2022-12-18 22:02:07 -0500

    update edit receipt

--- freelancer-front-end/arrancar-proyecto.sh
+++ freelancer-front-end/arrancar-proyecto.sh
@@ -1,5 +1,5 @@
 open -a Terminal && mongod --dbpath /Users/admin/Desktop/Jasem/Mongo &
-open -a Terminal && cd ~/Desktop/Jasem/React/Digitalia/ && npm start &
-open -a Terminal && cd ~/Desktop/Jasem/React/freelancer-front-end/freelancer-front-end/ && npm run dev &
+open -a Terminal && cd ~/Desktop/Jasem/React/freelancer/FreelancerApi2 && npm start &
+open -a Terminal && cd ~/Desktop/Jasem/React/freelancer/freelancer-front-end/ && npm run dev &
 # open -a /Applications/MongoDB\ Compass.app/Contents/MacOS/MongoDB\ Compass &
 # open -a /Applications/Postman.app/Contents/MacOS/Postman && fg
